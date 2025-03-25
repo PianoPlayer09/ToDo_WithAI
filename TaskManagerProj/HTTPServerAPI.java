a30d011a83b095b2d95a750b4f6c655838043025
PianoPlayer09 <141593018+PianoPlayer09@users.noreply.github.com>
2025-03-25 18:19:21 -0400

    Creating Files
    Adding Java base files, will add other parts later.

---
+++ TaskManagerProj/HTTPServerAPI.java
@@ -0,0 +1,11 @@
+package TaskManagerProj;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
